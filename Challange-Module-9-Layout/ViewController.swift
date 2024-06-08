d24e905550f3317b56abcfab8c2dd559c7846055
bristol <oro.eto.bristol@gmail.com>
2024-06-08 19:06:13 +0300

    Initial Commit

---
+++ Challange-Module-9-Layout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Challange-Module-9-Layout
+//
+//  Created by Apple M1 on 08.06.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
